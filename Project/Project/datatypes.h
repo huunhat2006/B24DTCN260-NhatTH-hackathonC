3d5c73d1849b9c610fc54919344aeb00fec77bf8
Huunhat123 <huunhat20052006@gmail.com>
2024-12-29 11:29:43 +0700

    29/12

---
+++ Project/Project/datatypes.h
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+typedef struct menu {
+	int id;
+	char nameCategory[50];
+} Category;
+Category category[50]= {
+	{1,"Thuc phap"},
+	{2,"Do gia dung"},
+	{3,"Van phong pham"},
+	{4,"The dien thoai"},
+};
